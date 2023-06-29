0cdc10313c06703dd1f74f703bd028efb6403566
Krish <95106927+TomasN12@users.noreply.github.com>
2023-06-29 18:45:40 -0400

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,12 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+  player1_name=localStorage.setItem();
+
+  player2_name=localStorage.setItem();
+
+    window.location = "game_page.html";
+}
+
